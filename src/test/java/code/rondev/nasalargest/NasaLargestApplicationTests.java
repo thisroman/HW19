825e018d14eb3c735e90dcbf82657adb0ab060ab
Roman <rn@intravision.dk>
2022-07-19 20:23:55 +0300

    Init

---
+++ src/test/java/code/rondev/nasalargest/NasaLargestApplicationTests.java
@@ -0,0 +1,13 @@
+package code.rondev.nasalargest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaLargestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
